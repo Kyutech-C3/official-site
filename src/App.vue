84f794fe29c99e1e0edacaacdc07e4c277e0dc03
rkun123 <tren111213@gmail.com>
2020-09-22 22:33:24 +0900

    [Add] Initialized Vue.js project /w Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <Footer />
   </div>
 </template>
 
