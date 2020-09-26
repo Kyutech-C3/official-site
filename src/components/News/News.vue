11d5e4016c5c43bb5630983917c3a5b26397c752
rkun123 <tren111213@gmail.com>
2020-09-27 00:52:05 +0900

    [ADD] Newsセクションの雛形を作成

---
+++ src/components/News/News.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NewsCard title="TestTitle" description="TestDescription" imageUrl="https://via.placeholder.com/150" />
+    </div>
+</template>
+<script>
+import NewsCard from './_NewsCard.vue'
+export default {
+    components: {
+        NewsCard
+    }
+}
+</script>
---
+++ src/components/News/_NewsCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img :src="imageUrl" />
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NewsCard',
+    props: {
+        title: String,
+        description: String,
+        imageUrl: String,
+    },
+}
+</script>
