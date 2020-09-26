dcebc07e1828e9659713790d620bdf15f23fba22
U3cSeroH1 <unkio0602@gmail.com>
2020-09-27 00:56:49 +0900

    23

---
+++ src/components/Activity/Activity.vue
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue.js - Hello world</title>
+</head>
+<body>
+
+    <div id="app">
+        {{ message }}
+    </div>
+
+    <script src="./vue.js"></script>
+    <script>
+  new Vue({
+      el: '#app',
+
+      data () {
+          return {
+              message: 'Hello world!'
+          }
+      }
+  })
+    </script>
+</body>
+</html>
