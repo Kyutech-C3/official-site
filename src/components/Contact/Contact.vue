753bd1d620053e793483f85837f75e580a75f888
MovingPointP <kobayashi.akio39@gmail.com>
2020-09-27 02:15:26 +0900

    ADD Home.vueとContact.vueのテスト

---
+++ src/components/Contact/Contact.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p>contact</p>
+  </div>
+</template>
