776f60e62eebcbdad671165705eb01a8c839b583
U3cSeroH1 <unkio0602@gmail.com>
2020-09-28 01:37:55 +0900

    [update]成型　定式化

--- src/components/Activity/Activity.vue
+++ src/components/Activity/Activity.vue
@@ -1,42 +1,17 @@
 ﻿<template>
     <div class="Activity">
 
-        <div class="description">
-            <p>{{bitch}}</p>
-        </div>
-
-
-        <div class="example">
-            <div class="example">
-                <video muted autoplay loop name="media" style="width: 75vw; border-radius: 20px; ">
-                    <source src="../../assets/b34c17a5e04fb74e38ab8f96335a8e03.mp4" type="video/webm">
-
-                </video>
-                <p>{{message}}</p>
-
+            <div class="example" v-for="item in items" :key="item">
+                <div>
+                    <video muted autoplay loop name="media">
+                        <source :src="item.movie" type="video/webm">
+                    </video>
+                </div>
+                <div style="text-align:center;">
+                    <h1 class="xtex">{{item.message}}</h1>
+                    <h6 class="ztex">{{item.description}}</h6>
+                </div>
             </div>
-            <div class="example">
-
-                <video muted autoplay loop name="media" style="width: 75vw; border-radius: 20px; ">
-                    <source src="../../assets/0951ad5c744a5a3f7ee01d2e5a9d5e90.mp4" type="video/webm">
-
-                </video>
-                <p>{{bitch}}</p>
-
-
-            </div>
-            <div class="example">
-
-                <video muted autoplay loop name="media" style="width: 75vw; border-radius: 20px; ">
-                    <source src="https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm" type="video/webm">
-
-                </video>
-                <p>{{bitch}}</p>
-
-
-            </div>
-
-        </div>
 
     </div>
 
@@ -44,18 +19,45 @@
 
 
 <script>
+
+    import img1 from '../../assets/0951ad5c744a5a3f7ee01d2e5a9d5e90.mp4';
+    import img2 from '../../assets/b34c17a5e04fb74e38ab8f96335a8e03.mp4';
+    import img3 from '../../assets/c12.mp4';
+
+
     export default ({
         name: 'Activity',
 
         data() {
+
             return {
-                message: 'ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！！仏父部武舞父リブブ',
-                bitch: 'おちｍぽ'
+                items: [
+                    {
+                        movie: img1,
+                        message: 'そんな',
+                        description: 'バナナバナナバナナバナナバナナバナナバナナバナナバナナ',
+
+                    },
+                    {
+                        movie: img2,
+                        message: 'アルミ缶のうえに',
+                        description: 'あるミカン',
+
+                    },
+                    {
+                        movie: img3,
+                        message: 'ここを',
+                        description: 'かえな',
+
+                    },
+                ]
             }
+
+
         },
 
 
-
+        
 
 
 
@@ -71,34 +73,42 @@
 
     }
 
-        .example p {
+        .example .xtex{
             position: absolute;
-            top: 30%;
+            top: 25%;
             left: 50%;
             -ms-transform: translate(-50%,-50%);
             -webkit-transform: translate(-50%,-50%);
-            font-size: 2em; /*サイズ2倍*/
+            font-size: 5vw; /*サイズ2倍*/
+         
             color: white; /*文字は白に*/
             transform: translate(-50%,-50%);
             margin: 0;
             padding: 0;
         }
-
-        .example video {
-            width: 100%;
-            height: 100%;
-            object-fit: cover; /* この一行を追加するだけ！ */
-            margin-bottom :5rem
+        .example .ztex{
+            position: absolute;
+            top: 45%;
+            left: 50%;
+            -ms-transform: translate(-50%,-50%);
+            -webkit-transform: translate(-50%,-50%);
+            font-size: 3vw; /*サイズ2倍*/
+            color: white; /*文字は白に*/
+            transform: translate(-50%,-50%);
+            margin: 0;
+            padding: 0;
+            width: 65vw;
         }
 
-    .examplevideo::before {
-        content: ' ';
-        background-color: rgba(0,0,0,.5);
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-    }
+        .example video {
+            object-fit: cover; /* この一行を追加するだけ！ */
+            margin-bottom: 5vw;
+            width: 85vw;
+            height: 50vw;
+            border-radius: 20px;
+            filter: brightness(60%);
+
+        }
+
 
 </style>
