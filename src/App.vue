fb230a286a481c0896a0f03132e231771008d84a
rkun123 <tren111213@gmail.com>
2020-10-04 00:41:58 +0900

    [update] 開発用にHome.vueを更新

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $text-color;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,47 @@
 <template>
-  <div>
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <p>mainpage</p>
+  <div class="container">
+    <Header />
+    <Top />
+    <News />
+    <Members />
+    <Activity />
+    <Contact />
+    <Footer />
   </div>
 </template>
 
 <script>
 
 // import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header/Header'
+import Top from '@/components/Top/Top'
+import News from '@/components/News/News'
+import Members from '@/components/Members/Members'
+import Activity from '@/components/Activity/Activity'
+import Contact from '@/components/Contact/Contact'
+import Footer from '@/components/Footer/Footer'
 
 export default {
-  // name: 'Home',
-  // components: {
-  //   HelloWorld
-  // }
+  components: {
+    Header,
+    Top,
+    News,
+    Members,
+    Activity,
+    Contact,
+    Footer,
+  }
 }
 </script>
+<style lang="scss" scoped>
+.container {
+  background-color: #ddd;
+}
+@media screen and (min-width: 1024px) {
+  .container {
+    margin-left: 50%;
+    transform: translateX(-50%);
+    width: 1024px;
+  }
+}
+</style>
