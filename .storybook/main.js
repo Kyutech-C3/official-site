257a973224ceb718649e34d3a30647f076a8a33c
rkun123 <tren111213@gmail.com>
2020-10-03 22:52:12 +0900

    [add] NewsCardのストーリーにknobsを追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs"
   ],
   webpackFinal: (config) => {
     const variablesScss = fs.readFileSync(path.resolve(__dirname, "../src/styles/_variables.scss")).toString()
--- src/stories/_NewsCard.stories.js
+++ src/stories/_NewsCard.stories.js
@@ -1,13 +1,43 @@
 import _NewsCard from '@/components/News/_NewsCard.vue'
 
+import {withKnobs, text, number, files} from '@storybook/addon-knobs'
+
 export default {
     title: 'components/NewsCard',
-    component: _NewsCard
+    component: _NewsCard,
+    decorators: [ withKnobs ]
 }
 
 const Primary = () => ({
     components: { _NewsCard },
-    template: '<_NewsCard></_NewsCard>'
+    props: {
+        title: {
+            default: text('Title', 'TestNews')
+        },
+        description: {
+            default: text('Description', 'foobar')
+        },
+        imageUrl: {
+            default: files('ImageUrl')
+        },
+        year: {
+            default: number('Year', 2020)
+        },
+        month: {
+            default: number('Month', 10)
+        },
+        day: {
+            default: number('Day', 3)
+        }
+    },
+    template: `<_NewsCard
+        :title="title"
+        :description="description"
+        :imageUrl="imageUrl"
+        :year="year"
+        :month="month"
+        :day="day"
+        ></_NewsCard>`
 })
 
 export const Default = Primary
