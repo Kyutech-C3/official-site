ae89c375de63b20e911d12b533890428765c0302
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-12 23:32:57 +0900

    [ADD] firestoreにタイトルと内容、日時を送信できた。でも画像はまだ

--- src/views/Addnews.vue
+++ src/views/Addnews.vue
@@ -1,25 +1,59 @@
 <template>
   <div>
-    <input v-model="title" placeholder="タイトル" class="title" required>
-    <br />
-    <textarea v-model="body" placeholder="内容" class="body" required></textarea>
-    <br />
-    <input type="date" name="num01" autocomplete="off" v-model="date">
-    <br />
-    <input type="file" name="example" accept="image/jpeg, image/png">
-    <br />
-    <button @click="console">確認</button>
+    <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+    >
+      <p>タイトルを入力してください</p>
+      <input v-model="title" placeholder="タイトル" class="title" required>
+      <br />
+      <p>Newsの内容を入力してください</p>
+      <textarea v-model="description" placeholder="内容" class="description" cols="30" rows="5" required></textarea>
+      <br />
+      <p>開催日時を教えてください</p>
+      <input type="date" name="num01" autocomplete="off" v-model="date" required>
+      <br />
+      <p>画像を挿入してください</p>
+      <input type="file" name="example" accept="image/jpeg, image/png" required>
+      <br />
+      <button @click="addNews">投稿</button>
+    </v-form>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+import 'firebase/firestore'
+
 export default {
   data: () => ({
+    db: null,
+    valid: true,
     title: "",
-    body: "",
+    description: "",
     date: ""
   }),
+  created: function () {
+    this.db = firebase.firestore()
+  },
   methods: {
+    addNews: function () {
+      // eslint-disable-next-line no-unused-vars
+      var _this = this
+      this.db.collection('news').add({
+        title: _this.title,
+        description: _this.description,
+        date: _this.date
+      })
+      .then(function () {
+        _this.title = ''
+        _this.description = ''
+      })
+      .catch(function () {
+        console.log('err')
+      })
+    },
     console  () {
       console.log(this.date)
     }
@@ -30,10 +64,13 @@ export default {
 <style scoped>
 .title{
   padding: 5px;
-  margin: 15px;
+  margin-bottom: 10px;
 }
 .body{
   padding: 5px;
-  margin: 15px;
+  margin-bottom: 10px;
+}
+button{
+  margin: 10px;
 }
 </style>
