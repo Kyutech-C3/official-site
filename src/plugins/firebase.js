1b3d9dcbb3d488eba01e4631676e7f8f04a8f734
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-19 15:27:43 +0900

    [update+add] plugins-frebase

---
+++ src/plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDRqehA2DSfntEBPoStYoukeTR8T2a0sZY",
+  authDomain: "c3-officialsite.firebaseapp.com",
+  databaseURL: "https://c3-officialsite.firebaseio.com",
+  projectId: "c3-officialsite",
+  storageBucket: "c3-officialsite.appspot.com",
+  messagingSenderId: "1054658532963",
+  appId: "1:1054658532963:web:4ea6ccd87cb94615184a56",
+  measurementId: "G-S3HT2Z6MX7"
+});
+
+export const Newsdb = firebaseApp.firestore().collection('news');
--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,38 +1,45 @@
 <template>
   <div class="signin">
     <h1>サインイン</h1>
-    <input type="text" placeholder="ユーザ名" v-model="username">
-    <input type="password" placeholder="パスワード" v-model="password">
+    <input type="text" placeholder="ユーザ名" v-model="username" />
+    <input type="password" placeholder="パスワード" v-model="password" />
     <button @click="signIn">サインイン</button>
-    <p>アカウント登録はしましたか? <br>
-      <router-link to="/signup">ここをクリックしてアカウント登録をしてください</router-link>
+    <p>
+      アカウント登録はしましたか? <br />
+      <router-link to="/signup"
+        >ここをクリックしてアカウント登録をしてください</router-link
+      >
     </p>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export default {
-  name: 'Signin',
-  data () {
+  name: "Signin",
+  data() {
     return {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: "",
+    };
   },
   methods: {
-    signIn: function () {
-      firebase.auth().signInWithEmailAndPassword(this.username, this.password).then(
-        user => {
-          alert('サインイン成功です。')
-          this.$router.push('/success')
-        },
-        error => {
-          alert(error.message)
-        }
-      )
-    }
-  }
-}
+    signIn: function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.username, this.password)
+        .then(
+          (user) => {
+            console.log(user);
+            alert("サインイン成功です。");
+            this.$router.push("/success");
+          },
+          (error) => {
+            alert(error.message);
+          }
+        );
+    },
+  },
+};
 </script>
