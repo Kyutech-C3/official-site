58bf8c8721db72ede273327464a98b6e17918d3d
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-26 17:05:08 +0900

    [fix] main.js firebase除去

--- src/main.js
+++ src/main.js
@@ -2,22 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import firebase from 'firebase'
+import './plugins/firebase.js'
 
 Vue.config.productionTip = false
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDRqehA2DSfntEBPoStYoukeTR8T2a0sZY",
-  authDomain: "c3-officialsite.firebaseapp.com",
-  databaseURL: "https://c3-officialsite.firebaseio.com",
-  projectId: "c3-officialsite",
-  storageBucket: "c3-officialsite.appspot.com",
-  messagingSenderId: "1054658532963",
-  appId: "1:1054658532963:web:4ea6ccd87cb94615184a56",
-  measurementId: "G-S3HT2Z6MX7"
-  }
-
-firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
