7fcab1de316d18a5e3f80a7b5adc6b1da5a326c8
rkun123 <tren111213@gmail.com>
2020-12-21 21:41:04 +0900

    [update] NewsView appearance

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <section class="news_header">
-      <p>投稿日: {{ news.date }}</p>
-    </section>
-    <section class="news_content">
-      <div ref="htmlContent" class="news_markdown"></div>
-    </section>
+  <div class="news_view">
+    <div class="news_container">
+      <section class="news_header">
+        <p>投稿日: {{ news.date }}</p>
+      </section>
+      <section class="news_content">
+        <div v-html="news.html" class="news_markdown">
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 <script>
@@ -24,7 +27,7 @@ export default {
       this.showContent = false;
     },
   },
-  mounted: function () {
+  created: function () {
     const newsId = this.$route.params.newsId
     console.log(newsId)
     if(newsId === null) this.$router.push('/')
@@ -35,13 +38,65 @@ export default {
       .get()
       .then((doc) => {
         self.news = doc.data()
-        self.$refs.htmlContent.innerHTML = self.news.html
       })
   },
 };
 </script>
 <style lang="scss" scoped>
-.news_markdown h1 {
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+.news_header {
   text-align: center;
+}
+.news_view {
+  text-align: center;
+}
+.news_view{
+  padding: 0 2rem;
+  .news_container {
+    text-align: left;
+  }
+}
+
+.news_markdown {
+  /deep/ h1:first-of-type{
+    font-size: 3em;
+    margin-top: 0;
+    text-align: center;
+  }
+
+  /deep/ h2, /deep/ h3 {
+    border-bottom: solid 2px $brand-color;
+  }
+
+  /deep/ img, video, iframe{
+    max-width: 100%;
+  }
+  /deep/ code {
+    font-family: 'jetBrains Mono', monospace;
+    background-color: $brand-color;
+    padding: .2em;
+    font-size: .85em;
+  }
+  /deep/ pre {
+    font-family: 'jetBrains Mono', monospace;
+    font-size: .85em;
+    padding: .5em;
+    background-color: $brand-color;
+    /deep/ code {
+      font-family: 'jetBrains Mono', monospace;
+      padding: 0;
+      background-color: $brand-color;
+      font-size: 1em;
+    }
+  }
+  /deep/ blockquote {
+    margin-left: 0;
+    padding-left: 1em;
+    border-left: solid 3px $brand-color;
+  }
+  /deep/ hr {
+    color: none;
+    border-bottom: solid 2px $brand-color;
+  }
 }
 </style>
